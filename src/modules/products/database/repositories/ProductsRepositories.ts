7266a98b3080be13422e0083691941b6e69e58f4
DevPlayer02 <contato.giovanicf@gmail.com>
2025-04-23 18:35:28 -0300

    Feat: Adding service show products

--- src/modules/products/database/repositories/ProductsRepositories.ts
+++ src/modules/products/database/repositories/ProductsRepositories.ts
@@ -4,5 +4,8 @@ import { Product } from '../entities/Product';
 export const productsRepositories = AppDataSource.getRepository(Product).extend({
   async findByName(name: string): Promise<Product | null> {
     return this.findOneBy({ name });
+  },
+  async findById(id: string): Promise<Product | null> {
+    return this.findOneBy({ id });
   }
 })
