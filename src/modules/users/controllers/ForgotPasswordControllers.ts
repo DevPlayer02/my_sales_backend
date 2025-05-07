4ecbd64ebba1bde4c5b71236820ae37aaf99eaf6
DevPlayer02 <contato.giovanicf@gmail.com>
2025-05-07 20:36:53 -0300

    Feat: Adding password controllers and routes

---
+++ src/modules/users/controllers/ForgotPasswordControllers.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body;
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService();
+
+    await sendForgotPasswordEmailService.execute({ email });
+
+    return res.status(204).json();
+  }
+}
+
