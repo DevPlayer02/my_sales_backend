7dbfeef1b63b1d19a668739a4c4895b71d09f430
DevPlayer02 <contato.giovanicf@gmail.com>
2025-05-09 20:11:41 -0300

    Feat: Adding customer services

---
+++ src/modules/customers/services/ListCustomersService.ts
@@ -0,0 +1,10 @@
+import { Customer } from "../database/entities/Customer";
+import { customerRepository } from "../database/repositories/CustomerRepositories";
+
+
+export default class ListCustomersService {
+  public async execute(): Promise<Customer[]> {
+    const customers = await customerRepository.find();
+    return customers;
+  }
+}
