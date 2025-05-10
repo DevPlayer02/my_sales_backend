77dc9a1d044deb13b462a1b46564cc83d0118bf4
DevPlayer02 <contato.giovanicf@gmail.com>
2025-05-10 16:25:47 -0300

    Feat: Addind order schema and routes

---
+++ src/modules/orders/routes/OrdersRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import OrdersControllers from '../controllers/OrdersControllers';
+import AuthMiddleware from '@shared/middlewares/authMiddleware';
+import { createOrderValidate, idParamsValidate } from '../schemas/OrdersSchemas';
+
+const ordersRouter = Router();
+const ordersController = new OrdersControllers();
+
+ordersRouter.use(AuthMiddleware.execute);
+ordersRouter.get('/:id', idParamsValidate, ordersController.show);
+ordersRouter.post('/', createOrderValidate, ordersController.create);
+
+export default ordersRouter;
