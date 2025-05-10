0daffcb488e35033f6efacb84a3024712da69d24
DevPlayer02 <contato.giovanicf@gmail.com>
2025-05-10 14:42:41 -0300

    Feat: Adding order controllers

---
+++ src/modules/orders/controllers/OrdersControllers.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import ShowOrderService from '../services/ShowOrderService';
+import CreateOrderService from '../services/CreateOrderService';
+
+export default class OrdersControllers {
+  async show(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const showOrder = new ShowOrderService();
+
+    const order = await showOrder.execute(id);
+
+    return res.json(order);
+  }
+
+  async create(req: Request, res: Response): Promise<Response> {
+    const { customer_id, products } = req.body;
+
+    const createOrder = new CreateOrderService();
+
+    const order = await createOrder.execute({ customer_id, products });
+
+    return res.json(order);
+  }
+}
