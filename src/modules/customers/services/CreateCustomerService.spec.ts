dded07e9d13a4b2742770b7a17b505ded5509f55
DevPlayer02 <contato.giovanicf@gmail.com>
2025-05-17 14:35:27 -0300

    Feat: Adding jest config

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,9 @@
+describe('CreateCustomerService', () => {
+  it('should be able to create a new customer', () => {
+    expect(5 + 5).toBe(10);
+  });
+
+  it('should not be able to create a new customer with email that is alerady in use', () => {
+    expect(5 + 5).toBe(10);
+  });
+});
